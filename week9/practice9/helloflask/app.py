1facf47066d9f6c345117ae68e9d31df47d98cac
iz00 <isadoracontsost@gmail.com>
2024-05-20 22:37:53 -0300

    Organize files

---
+++ week9/practice9/helloflask/app.py
@@ -0,0 +1,15 @@
+# Learning how to build a simple flask app
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        print("Form submitted!")
+        color = request.form.get("color")
+        return render_template("color.html", color=color)
